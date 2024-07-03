79e2f5e1097fa6c03ef64f6ec041c23348f4e761
Oleg Shilov <me@olegshilov.com>
2024-07-03 18:59:33 +0700

    chore: cleanup

--- libs/atlas-proto/src/lib/atlas-proto.spec.ts
+++
@@ -1,7 +0,0 @@
-import { atlasProto } from './atlas-proto';
-
-describe('atlasProto', () => {
-  it('should work', () => {
-    expect(atlasProto()).toEqual('atlas-proto');
-  });
-});
--- libs/atlas-proto/src/lib/atlas-proto.ts
+++
@@ -1,3 +0,0 @@
-export function atlasProto(): string {
-  return 'atlas-proto';
-}
