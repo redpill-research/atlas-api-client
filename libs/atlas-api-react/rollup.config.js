9c16bd5eda16fa70959113084e05a4df33d6f9e8
Oleg Shilov <me@olegshilov.com>
2024-07-31 20:38:51 +0700

    chore(deps): downgrade `@bufbuild/protobuf` package

--- libs/atlas-api-react/rollup.config.js
+++ libs/atlas-api-react/rollup.config.js
@@ -15,7 +15,7 @@ module.exports = withNx(
   },
   {
     // Provide additional rollup configuration here. See: https://rollupjs.org/configuration-options
-    output: { sourcemap: true },
+    output: { sourcemap: false },
     plugins: [
       svg({
         svgo: false,
