11395efdd633f72b66ad1e271346bc4c72d09600
Oleg Shilov <me@olegshilov.com>
2024-08-05 15:03:46 +0700

    test: collect coverage and pass without tests

--- jest.preset.js
+++ jest.preset.js
@@ -1,3 +1,8 @@
 const nxPreset = require('@nx/jest/preset').default;
 
-module.exports = { ...nxPreset };
+module.exports = {
+  ...nxPreset,
+  coverageReporters: ['text', 'json', 'cobertura'],
+  collectCoverageFrom: ['./src/**'],
+  passWithNoTests: true,
+};
